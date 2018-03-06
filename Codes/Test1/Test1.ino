c8db7c0296b691a920d6eaa89548e0f04595f007
Jorge <jorge.david.zepmed@gmail.com>
2018-03-06 01:24:47 -0600

    final1

--- Codes/Test1/Test1.ino
+++ Codes/Test1/Test1.ino
@@ -14,49 +14,80 @@ Servo ceja_izq;  //Llamamos como "ceja_izq" al servo que hace el moviemiento de 
 Servo ceja_der;  //Llamamos como "ceja_der" al servo que hace el moviemiento de la ceja derecha (viendolo tu de frente)
 
 //Joystick
-int horX =0;      
-int verY =1;    
+int joyX =0;
+int joyY =1;
 
 int horPos = 90;
 int verPos = 90;
 
 //Nombres de botones
-const int bot1 = 6;
-const int bot2 = 7;
-const int bot3 = 8;
+ int bot1 = 6;
+ int bot2 = 7;
+ int bot3 = 8;
 
 //Variables de botones
 int bot1_state = 0;
 int bot2_state = 0;
 int bot3_state = 0;
-boolean state;
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.printIn("Servo test");
-  
+  Serial.print("Servo test");
+
   //Servos
   ojoX.attach(1);
   ojoY.attach(2);
-  boca..attach(3);
+  boca.attach(3);
   ceja_izq.attach(4);
   ceja_der.attach(5);
   pinMode(bot1,INPUT);
   pinMode(bot2,INPUT);
   pinMode(bot3,INPUT);
- 
+
 }
 
 void loop()
 {
   //Servos
-  horPos = analogRead
-  
+  horPos = analogRead(joyX);
+  verPos = analogRead(joyY);
+  horPos = map(horPos, 0,1023,0,180);
+  verPos = map(verPos, 0,1023,0,180);
+  Serial.print(horPos);
+  Serial.print("\t");
+  Serial.print(verPos);
+
+  ojoX.write(horPos);
+  ojoY.write(verPos);
+  delay(50);
+
   //Botones
   bot1_state = digitalRead(bot1);
   bot2_state = digitalRead(bot2);
   bot3_state = digitalRead(bot3);
-  
-  
+
+//Movimiento de boca
+
+  if(bot1_state == HIGH)
+  {
+    boca.write(160);
+  }
+  if (bot1_state == LOW)
+  {
+    boca.write(90);
+  }
+
+//Cejas
+if (bot2_state == HIGH)
+{
+  ceja_der.write(110);
+  ceja_izq.write(110);
+}
+if (bot2_state == LOW)
+{
+  ceja_der.write(90);
+  ceja_izq.write(90);
+}
+
 }
